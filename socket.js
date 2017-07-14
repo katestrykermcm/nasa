bd4ee0772b938c2fef11aa6fca952459bf4d84fd
Shunta McDavid <shuntamcd@gmail.com>
2017-07-13 17:29:01 -0700

    fixed notification

--- socket.js
+++ socket.js
@@ -167,9 +167,23 @@ if (!('webkitSpeechRecognition' in window)) {
         //returns true or false...
         var exists = pattern.test(message);
         if(exists){//true statement, do whatever
-  			$( function() {
-				$( "#dialog" ).dialog();
-			  });
+ 			// Get the modal
+			var modal = document.getElementById('myModal');
+			modal.style.display = "block";
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+				modal.style.display = "none";
+			}
+
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
         }
         else{//false statement..do whatever
 			
